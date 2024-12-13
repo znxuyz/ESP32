4064a8f24fa691e7560231a9c16d32656e285c22
znxuyz <znxuyz103111@gmail.com>
2024-12-13 13:25:13 +0800

    main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+// micro:bit MakeCode Extension for ESP32 WiFi and Initialization
+
 namespace ESP32 {
 
     let wifiConnected = false;
@@ -43,6 +45,78 @@ namespace ESP32 {
     }
 
     /**
+     * Send an HTTP GET request to a specific URL
+     * @param url The URL to send the GET request to
+     */
+    //% block="發送 HTTP GET 到網址 %url"
+    export function sendHttpGet(url: string): void {
+        serial.writeString("AT+HTTPCLIENT=2,0,\"" + url + "\",,,1\r\n");
+        basic.pause(3000);
+        let response = serial.readString();
+        serial.writeLine(response);
+    }
+
+    /**
+     * Send an HTTP POST request with data
+     * @param url The URL to send the POST request to
+     * @param data The data to include in the POST request
+     */
+    //% block="發送 HTTP POST 到網址 %url，數據 %data"
+    export function sendHttpPost(url: string, data: string): void {
+        serial.writeString("AT+HTTPCLIENT=2,1,\"" + url + "\",,,2,\"" + data + "\"\r\n");
+        basic.pause(3000);
+        let response = serial.readString();
+        serial.writeLine(response);
+    }
+
+    /**
+     * Transmit data from micro:bit sensors to a server
+     * @param url The server URL
+     * @param sensorData The data to send (e.g., sensor readings)
+     */
+    //% block="傳輸數據到伺服器 %url，數據 %sensorData"
+    export function transmitSensorData(url: string, sensorData: string): void {
+        let data = "sensorData=" + sensorData;
+        serial.writeString("AT+HTTPCLIENT=2,1,\"" + url + "\",,,2,\"" + data + "\"\r\n");
+        basic.pause(3000);
+        let response = serial.readString();
+        serial.writeLine(response);
+    }
+
+    /**
+     * Control GPIO pin state
+     * @param pin GPIO pin number
+     * @param state GPIO state (true for HIGH, false for LOW)
+     */
+    //% block="設置 GPIO 腳位 %pin 為 %state"
+    export function setGPIO(pin: number, state: boolean): void {
+        serial.writeString("AT+GPIO=" + pin + "," + (state ? "1" : "0") + "\r\n");
+        basic.pause(100);
+    }
+
+    /**
+     * Start ESP32 in Access Point (AP) mode
+     * @param ssid Access Point SSID
+     * @param password Access Point Password
+     */
+    //% block="啟用 AP 模式，名稱 %ssid，密碼 %password"
+    export function startAccessPoint(ssid: string, password: string): void {
+        serial.writeString("AT+CWSAP=\"" + ssid + "\",\"" + password + "\",5,3\r\n");
+        basic.pause(1000);
+    }
+
+    /**
+     * Get the IP address of the ESP32
+     * @return The IP address as a string
+     */
+    //% block="獲取 ESP32 的 IP 地址"
+    export function getIPAddress(): string {
+        serial.writeString("AT+CIFSR\r\n");
+        basic.pause(1000);
+        return serial.readString();
+    }
+
+    /**
      * Send an AT command to the ESP32 module and get the response
      * @param command AT command to send
      * @return Response from the ESP32 module
@@ -54,3 +128,4 @@ namespace ESP32 {
         return serial.readString();
     }
 }
+
