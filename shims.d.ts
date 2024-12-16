633c6d69af5c28b229ccb76ef421c34005646358
znxuyz <znxuyz103111@gmail.com>
2024-12-16 15:15:47 +0800

    Create shims.d.ts

---
+++ shims.d.ts
@@ -0,0 +1,20 @@
+//% color=#0fbc11 icon="\uf1eb" block="ESP32WiFi"
+declare namespace ESP32WiFi {
+    //% block="初始化 ESP32"
+    function init(): void;
+
+    //% block="連接 WiFi SSID %ssid 密碼 %password"
+    function connectWiFi(ssid: string, password: string): void;
+
+    //% block="確認 WiFi 狀態"
+    function checkWiFi(): boolean;
+
+    //% block="傳送資料到 Google Sheets 資料 %data"
+    function sendToGoogleSheets(data: string): void;
+
+    //% block="傳送資料到 MIT App 資料 %data"
+    function sendToMITApp(data: string): void;
+
+    //% block="啟動 Web 伺服器"
+    function startWebServer(): void;
+}
